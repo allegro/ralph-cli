a1eee63bc8d72c954607263ae005bbb4693cb8d9
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-03-27 16:42:22 +0100

    clean up code

--- src/beast/beast.py
+++ src/beast/beast.py
@@ -6,8 +6,7 @@
 Documentation on https://github.com/allegro/ralph_beast/
 
 Usage:
- beast export <resource> [--filter=filter_expression] [--fields=fields]
- [--csv] [--yaml] [--trim] [--limit=limit]
+ beast export <resource> [--filter=filter_expression] [--fields=fields] [--csv] [--yaml] [--trim] [--limit=limit]
  beast update <resource> <id> <fields> <fields_values>
  beast export
  beast inspect [--resource=resource]
@@ -175,8 +174,8 @@ def export(arguments, settings):
     csv_export = arguments.get('--csv')
     trim_columns = arguments.get('--trim')
     if not resource:
-        print """Resource not specified. Type beast inspect [resource] to
-                                                inspect available fields."""
+        print "Resource not specified. Type beast inspect [resource] to"
+              "inspect available fields."
         return inspect(arguments, settings)
     data = get_resource(settings, resource, limit)
     result_data = []

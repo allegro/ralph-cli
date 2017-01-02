f926e4ab29affd12eea31e94c29308911d829350
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2017-01-02 11:52:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ rm -rf ~/.beast/virtual 2>/dev/null
 rm -rf ~/.beast/beast 2>/dev/null
 rm -f ~/beast 2>/dev/null
 
-curl https://raw.github.com/pypa/virtualenv/master/virtualenv.py >/tmp/virtualenv.py
+
+curl https://raw.githubusercontent.com/pypa/virtualenv/master/virtualenv.py >/tmp/virtualenv.py
 python2.7 /tmp/virtualenv.py --no-site-packages ~/.beast/virtual/ 2>/dev/null
 source ~/.beast/virtual/bin/activate
 
